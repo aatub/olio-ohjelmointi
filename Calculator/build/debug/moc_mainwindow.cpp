a32fd806fc2a0fa8d24dd8d739e87fe3ac1e2de2
Aatu Böhm <67936837+aatub@users.noreply.github.com>
2025-02-23 22:16:34 +0200

    Viikkotehtävä 7 valmis (Laskin)

--- Calculator/build/debug/moc_mainwindow.cpp
+++ Calculator/build/debug/moc_mainwindow.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../mainwindow.h"
+#include "../../../viikkoteht7/Calculator/mainwindow.h"
 #include <QtCore/qmetatype.h>
 
 #include <QtCore/qtmochelpers.h>
@@ -38,9 +38,10 @@ struct qt_meta_tag_ZN10MainWindowE_t {};
 #ifdef QT_MOC_HAS_STRINGDATA
 static constexpr auto qt_meta_stringdata_ZN10MainWindowE = QtMocHelpers::stringData(
     "MainWindow",
-    "countBtn",
+    "numberClickedHandler",
     "",
-    "resetBtn"
+    "clearAndEnterClickHandler",
+    "addSubMulDivClickHandler"
 );
 #else  // !QT_MOC_HAS_STRINGDATA
 #error "qtmochelpers.h not found or too old."
@@ -52,7 +53,7 @@ Q_CONSTINIT static const uint qt_meta_data_ZN10MainWindowE[] = {
       12,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -60,10 +61,12 @@ Q_CONSTINIT static const uint qt_meta_data_ZN10MainWindowE[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   26,    2, 0x08,    1 /* Private */,
-       3,    0,   27,    2, 0x08,    2 /* Private */,
+       1,    0,   32,    2, 0x08,    1 /* Private */,
+       3,    0,   33,    2, 0x08,    2 /* Private */,
+       4,    0,   34,    2, 0x08,    3 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
 
@@ -79,9 +82,11 @@ Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
     qt_incomplete_metaTypeArray<qt_meta_tag_ZN10MainWindowE_t,
         // Q_OBJECT / Q_GADGET
         QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
-        // method 'countBtn'
+        // method 'numberClickedHandler'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'resetBtn'
+        // method 'clearAndEnterClickHandler'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'addSubMulDivClickHandler'
         QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
     nullptr
@@ -92,8 +97,9 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
     auto *_t = static_cast<MainWindow *>(_o);
     if (_c == QMetaObject::InvokeMetaMethod) {
         switch (_id) {
-        case 0: _t->countBtn(); break;
-        case 1: _t->resetBtn(); break;
+        case 0: _t->numberClickedHandler(); break;
+        case 1: _t->clearAndEnterClickHandler(); break;
+        case 2: _t->addSubMulDivClickHandler(); break;
         default: ;
         }
     }
@@ -119,14 +125,14 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 3;
     }
     if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 3)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 2;
+        _id -= 3;
     }
     return _id;
 }
--- Calculator/build/ui_mainwindow.h
+++ Calculator/build/ui_mainwindow.h
@@ -11,6 +11,7 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
@@ -24,9 +25,28 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
-    QLineEdit *lineEdit;
-    QPushButton *countButton;
+    QLineEdit *num1;
+    QPushButton *oneButton;
     QPushButton *resetButton;
+    QLineEdit *num2;
+    QLineEdit *result1;
+    QPushButton *sixButton;
+    QPushButton *fiveButton;
+    QPushButton *fourButton;
+    QPushButton *threeButton;
+    QPushButton *twoButton;
+    QPushButton *nineButton;
+    QPushButton *eightButton;
+    QPushButton *sevenButton;
+    QPushButton *zeroButton;
+    QPushButton *mul;
+    QPushButton *sub;
+    QPushButton *add;
+    QPushButton *enterButton;
+    QPushButton *div;
+    QLabel *label;
+    QLabel *label_2;
+    QLabel *label_3;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -37,15 +57,72 @@ public:
         MainWindow->resize(800, 600);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
-        lineEdit = new QLineEdit(centralwidget);
-        lineEdit->setObjectName("lineEdit");
-        lineEdit->setGeometry(QRect(430, 250, 113, 24));
-        countButton = new QPushButton(centralwidget);
-        countButton->setObjectName("countButton");
-        countButton->setGeometry(QRect(320, 230, 80, 24));
+        num1 = new QLineEdit(centralwidget);
+        num1->setObjectName("num1");
+        num1->setGeometry(QRect(180, 180, 113, 24));
+        oneButton = new QPushButton(centralwidget);
+        oneButton->setObjectName("oneButton");
+        oneButton->setGeometry(QRect(180, 230, 80, 24));
         resetButton = new QPushButton(centralwidget);
         resetButton->setObjectName("resetButton");
-        resetButton->setGeometry(QRect(320, 270, 80, 24));
+        resetButton->setGeometry(QRect(270, 320, 80, 24));
+        num2 = new QLineEdit(centralwidget);
+        num2->setObjectName("num2");
+        num2->setGeometry(QRect(310, 180, 113, 24));
+        result1 = new QLineEdit(centralwidget);
+        result1->setObjectName("result1");
+        result1->setGeometry(QRect(440, 180, 113, 24));
+        sixButton = new QPushButton(centralwidget);
+        sixButton->setObjectName("sixButton");
+        sixButton->setGeometry(QRect(360, 260, 80, 24));
+        fiveButton = new QPushButton(centralwidget);
+        fiveButton->setObjectName("fiveButton");
+        fiveButton->setGeometry(QRect(270, 260, 80, 24));
+        fourButton = new QPushButton(centralwidget);
+        fourButton->setObjectName("fourButton");
+        fourButton->setGeometry(QRect(180, 260, 80, 24));
+        threeButton = new QPushButton(centralwidget);
+        threeButton->setObjectName("threeButton");
+        threeButton->setGeometry(QRect(360, 230, 80, 24));
+        twoButton = new QPushButton(centralwidget);
+        twoButton->setObjectName("twoButton");
+        twoButton->setGeometry(QRect(270, 230, 80, 24));
+        nineButton = new QPushButton(centralwidget);
+        nineButton->setObjectName("nineButton");
+        nineButton->setGeometry(QRect(360, 290, 80, 24));
+        eightButton = new QPushButton(centralwidget);
+        eightButton->setObjectName("eightButton");
+        eightButton->setGeometry(QRect(270, 290, 80, 24));
+        sevenButton = new QPushButton(centralwidget);
+        sevenButton->setObjectName("sevenButton");
+        sevenButton->setGeometry(QRect(180, 290, 80, 24));
+        zeroButton = new QPushButton(centralwidget);
+        zeroButton->setObjectName("zeroButton");
+        zeroButton->setGeometry(QRect(180, 320, 80, 24));
+        mul = new QPushButton(centralwidget);
+        mul->setObjectName("mul");
+        mul->setGeometry(QRect(450, 290, 80, 24));
+        sub = new QPushButton(centralwidget);
+        sub->setObjectName("sub");
+        sub->setGeometry(QRect(450, 260, 80, 24));
+        add = new QPushButton(centralwidget);
+        add->setObjectName("add");
+        add->setGeometry(QRect(450, 230, 80, 24));
+        enterButton = new QPushButton(centralwidget);
+        enterButton->setObjectName("enterButton");
+        enterButton->setGeometry(QRect(360, 320, 80, 24));
+        div = new QPushButton(centralwidget);
+        div->setObjectName("div");
+        div->setGeometry(QRect(450, 320, 80, 24));
+        label = new QLabel(centralwidget);
+        label->setObjectName("label");
+        label->setGeometry(QRect(180, 150, 71, 16));
+        label_2 = new QLabel(centralwidget);
+        label_2->setObjectName("label_2");
+        label_2->setGeometry(QRect(310, 150, 61, 16));
+        label_3 = new QLabel(centralwidget);
+        label_3->setObjectName("label_3");
+        label_3->setGeometry(QRect(440, 150, 49, 16));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
@@ -63,8 +140,25 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
-        countButton->setText(QCoreApplication::translate("MainWindow", "Count", nullptr));
+        oneButton->setText(QCoreApplication::translate("MainWindow", "1", nullptr));
         resetButton->setText(QCoreApplication::translate("MainWindow", "Reset", nullptr));
+        sixButton->setText(QCoreApplication::translate("MainWindow", "6", nullptr));
+        fiveButton->setText(QCoreApplication::translate("MainWindow", "5", nullptr));
+        fourButton->setText(QCoreApplication::translate("MainWindow", "4", nullptr));
+        threeButton->setText(QCoreApplication::translate("MainWindow", "3", nullptr));
+        twoButton->setText(QCoreApplication::translate("MainWindow", "2", nullptr));
+        nineButton->setText(QCoreApplication::translate("MainWindow", "9", nullptr));
+        eightButton->setText(QCoreApplication::translate("MainWindow", "8", nullptr));
+        sevenButton->setText(QCoreApplication::translate("MainWindow", "7", nullptr));
+        zeroButton->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        mul->setText(QCoreApplication::translate("MainWindow", "*", nullptr));
+        sub->setText(QCoreApplication::translate("MainWindow", "-", nullptr));
+        add->setText(QCoreApplication::translate("MainWindow", "+", nullptr));
+        enterButton->setText(QCoreApplication::translate("MainWindow", "Enter", nullptr));
+        div->setText(QCoreApplication::translate("MainWindow", "/", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Number 1", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "Number 2", nullptr));
+        label_3->setText(QCoreApplication::translate("MainWindow", "Result", nullptr));
     } // retranslateUi
 
 };
